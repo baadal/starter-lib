ac48677d3becbd0b7aed9cdad8800da73ab015cf
Abhishek Raj <abhi@raj.me>
2021-09-21 13:59:27 +0530

    Basic package setup

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        // Do not specify `targets` here, rather use browserlist (currently in package.json)
+        // since targets mentioned here informs it only to babel-loader, and not to webpack
+        useBuiltIns: "usage",
+        corejs: { version: "3.18", proposals: true },
+        shippedProposals: true,
+      }
+    ],
+    "@babel/preset-typescript"
+  ],
+  plugins: [
+    // "@babel/plugin-transform-runtime", // https://github.com/babel/babel/issues/6629#issuecomment-416986687
+    // "@babel/plugin-proposal-class-properties", // https://babeljs.io/docs/en/babel-preset-env#shippedproposals
+  ]
+};
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+module.exports = (env) => {
+  const outDir = env.ESM ? 'dist/esm' : 'dist/cjs';
+  const libraryType = env.ESM ? 'module' : 'commonjs';
+  const experiments = env.ESM ? { outputModule: true } : {};
+
+  const stats = {
+    // timings: false,
+    hash: false,
+    version: false,
+    builtAt: false,
+    assets: false,
+    entrypoints: false,
+    modules: false,
+    chunks: false,
+    children: false
+  };
+
+  return {
+    mode: 'production',
+    entry: {
+      'index': './src/index.ts',
+    },
+    experiments,
+    output: {
+      filename: '[name].js',
+      path: path.resolve(process.cwd(), outDir),
+      library: {
+        type: libraryType,
+      },
+    },
+    externalsPresets: {
+      node: true, // Target node environment (ignore built-in modules like path, fs, etc.)
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    devtool: 'source-map',
+    stats,
+  };
+};
